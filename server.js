5182fb653380b1608800a536511af4681c9ce140
SarahJedlaoui <sarajedlaoui999@gmail.com>
2025-02-27 15:40:54 +0100

    commit

--- server.js
+++ server.js
@@ -18,12 +18,13 @@ app.use(cors({
 }));
 
 // Connect to MongoDB
-mongoose.connect(mongoURI, {
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverSelectionTimeoutMS: 5000,  // Timeout after 5s instead of 10s
     socketTimeoutMS: 45000,  // Close sockets after 45s if no response
-})
+  })
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));
   
